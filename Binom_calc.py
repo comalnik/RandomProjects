49cb62cf43a0f01bd6f7841dbbd0123db79029a4
Luka G <gostencnikluka@gmail.com>
2022-10-27 08:12:38 +0200

    Improvements

--- Binom_calc.py
+++ Binom_calc.py
@@ -1,9 +1,23 @@
 import math
 #(a+b)^n
-a = int(input("a: "))
-b = int(input("b: "))
+#print("(a+b)^n")
+
+ques = input("Display values for 1 [Y]es/[N]o: ").upper()
+
+if ques == "Y":
+	print("(1+1)^n")
+	a = 1
+	b = 1
+else:
+	print("(a+b)^n")
+	a = int(input("a: "))
+	b = int(input("b: "))
+
+
 n = int(input("n: "))
 k = 0
+
+print("("+str(a)+"+"+str(b)+")"+"^"+str(n))
 
 list = []
 result = 0
@@ -15,5 +29,26 @@ for i in range(n):
 		list.append(res)
 		result = result + res
 
-print(list)
-print(result)
+mystr = ""
+for i in range(len(list)):
+	x=list[i]
+	if str(x)[0] == "-":
+		x = "("+str(x)+")"
+	if i != 0:
+		mystr += " + "+ str(x)
+		
+	else:
+		 mystr += ""+ str(x)
+
+
+if ques == "Y":
+	#print(list)
+	print(mystr)
+
+else:
+	
+	#print(list)
+	#stri = " + ".join(map(str, list))
+	#print(stri)
+	print(result)
+	print(mystr)
