c3559815f737609710407857ee0f35d64637c72f
Luka Gostenčnik <gostencnikluka@proton.me>
2024-09-07 22:39:48 +0200

    more trahs

--- shitty_rofi_pass.sh
+++ shitty_rofi_pass.sh
@@ -6,6 +6,6 @@ cd "$HOME"/.password-store || exit 0
 pass_entries=$(find ./* -type f | cut -c 3- | awk -F ".gpg" '{print $1}')
 pass_entry=$(echo -e "$pass_entries" | rofi -dmenu -i -p "Password") || exit 0
 pe1=$(pass "$pass_entry")
-line=$(printf '%s\n' "${pe1}" | rofi -dmenu -p "Enter to copy")
-printf '%s' "${line}" | xclip -sel clip
+line=$(printf '%s\n' "pass" | rofi -dmenu -p "Enter to copy")
+printf '%s' "${pe1}" | xclip -sel clip
 sleep $clip_clear; printf '%s' "" | xclip -sel clip 
